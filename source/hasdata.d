760e509fd1ce99691d1bad70a495dd17d3320315
WebFreak001 <janju007@web.de>
2017-01-21 13:24:59 +0100

    Initial commit

---
+++ source/hasdata.d
@@ -0,0 +1,30 @@
+module hasdata;
+
+import std.stdio : File;
+
+/// Returns true if the file or console handle has data available for read
+bool hasData()(File file) @system
+{
+	version (Posix)
+	{
+		import core.sys.posix.poll : pollfd, poll, POLLIN;
+
+		pollfd fds;
+		fds.fd = file.fileno;
+		fds.events = POLLIN;
+		auto ret = poll(&fds, 1, 0);
+		return ret == 1;
+	}
+	else version (Windows)
+	{
+		import core.sys.windows.windows : GetFileType, WaitForSingleObject,
+			FILE_TYPE_CHAR, WAIT_OBJECT_0;
+
+		if (GetFileType(file.windowsHandle) == FILE_TYPE_CHAR)
+			return WaitForSingleObject(file.windowsHandle, 0) == WAIT_OBJECT_0;
+		else
+			return !file.eof;
+	}
+	else
+		static assert(0, "No hasData implementation for this platform");
+}
